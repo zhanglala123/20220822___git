22da8841be2663c1e86752b58f6c2610b0516794
zhangchengcheng <m17319158124@163.com>
2022-08-27 00:17:00 +0800

    yaml加注释

--- utils/commlib.py
+++ utils/commlib.py
@@ -1,6 +1,37 @@
 # -*- coding: UTF-8 -*-
 import yaml
+"""
+1、zip() 函数用于将可迭代的对象作为参数，将对象中对应的元素打包成一个个元组，然后返回由这些元组组成的列表。
+如果各个迭代器的元素个数不一致，则返回列表长度与最短的对象相同，利用 * 号操作符，可以将元组解压为列表
+>>> a = [1,2,3]
+>>> b = [4,5,6]
+>>> c = [4,5,6,7,8]
+>>> zipped = zip(a,b)     # 返回一个对象
+>>> zipped
+<zip object at 0x103abc288>
+>>> list(zipped)  # list() 转换为列表
+[(1, 4), (2, 5), (3, 6)]
+>>> list(zip(a,c))              # 元素个数与最短的列表一致
+[(1, 4), (2, 5), (3, 6)]
 
+>>> a1, a2 = zip(*zip(a,b))          # 与 zip 相反，zip(*) 可理解为解压，返回二维矩阵式
+>>> list(a1)
+[1, 2, 3]
+>>> list(a2)
+[4, 5, 6]
+2、get函数用法
+#!/usr/bin/python
+
+tinydict = {'Name': 'Runoob', 'Age': 27}
+
+print ("Age : ", tinydict.get('Age'))
+
+# 没有设置 Sex，也没有设置默认的值，输出 None
+print ("Sex : ", tinydict.get('Sex'))  
+
+# 没有设置 Salary，输出默认的值  0.0
+print ('Salary: ', tinydict.get('Salary', 0.0))
+"""
 # 读取yaml文件
 def get_test_data(test_data_path):
     case = []  # 存储测试用例名称
